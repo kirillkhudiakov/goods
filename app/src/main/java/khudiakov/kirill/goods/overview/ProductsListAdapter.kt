3e80be53f3e7c1e1428297722e402b1bbe34bdb0
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-07 23:16:06 +0300

    Add ListAdapter for RecyclerView

---
+++ app/src/main/java/khudiakov/kirill/goods/overview/ProductsListAdapter.kt
@@ -0,0 +1,52 @@
+package khudiakov.kirill.goods.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import khudiakov.kirill.goods.data.Product
+import khudiakov.kirill.goods.databinding.ListItemBinding
+
+class ProductsListAdapter(private val clickListener: OnClickListener) :
+    ListAdapter<Product, ProductsListAdapter.ProductViewHolder>(ProductDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.bind(getItem(position), clickListener)
+    }
+
+    class ProductViewHolder private constructor(private val binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(product: Product, clickListener: OnClickListener) {
+            binding.product = product
+            binding.clickListener = clickListener
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ProductViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = ListItemBinding.inflate(inflater, parent, false)
+                return ProductViewHolder(binding)
+            }
+        }
+    }
+
+    class OnClickListener(private val clickListener: (product: Product) -> Unit) {
+        fun onClick(product: Product) = clickListener(product)
+    }
+
+    companion object ProductDiffCallback : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
