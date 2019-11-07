2f44e229189f271f835e7383469986d383a9413b
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-07 19:14:55 +0300

    Add moshi class annotation

--- app/src/main/java/khudiakov/kirill/goods/data/network/NetworkGood.kt
+++ app/src/main/java/khudiakov/kirill/goods/data/network/NetworkGood.kt
@@ -1,7 +1,9 @@
 package khudiakov.kirill.goods.data.network
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class NetworkGood(
     @Json(name = "gcode") var id: Long,
     var name: String,
