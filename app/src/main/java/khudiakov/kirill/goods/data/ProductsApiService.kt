8b5fb902de210833fb81d97f780e581ac19d32ec
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-08 14:22:21 +0300

    Instantiate basic Api service with Retrofit and Moshi

---
+++ app/src/main/java/khudiakov/kirill/goods/data/ProductsApiService.kt
@@ -0,0 +1,23 @@
+package khudiakov.kirill.goods.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "http://vimos.ru:1455"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ProductsApiService {
+    @GET("products?cat_id=7&limit=10&offset=0&base_id=12&sort_type=0")
+    fun getProducts()
+}
+
+object ProductsApi {
+    val retrofitService : ProductsApiService by lazy {
+        retrofit.create(ProductsApiService::class.java)
+    }
+}
