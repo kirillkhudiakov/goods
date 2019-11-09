0e459b1f4a4287ba30f73136f7e1322c73533df3
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-09 21:11:51 +0300

    Implement main view model

--- app/src/main/java/khudiakov/kirill/goods/overview/OverviewViewModel.kt
+++ app/src/main/java/khudiakov/kirill/goods/overview/OverviewViewModel.kt
@@ -1,6 +1,33 @@
 package khudiakov.kirill.goods.overview
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.Disposable
+import khudiakov.kirill.goods.data.Product
+import khudiakov.kirill.goods.data.ProductsApi
 
 class OverviewViewModel : ViewModel() {
+
+    private var disposableObserver: Disposable? = null
+
+    private val _products = MutableLiveData<List<Product>>()
+    val products: LiveData<List<Product>>
+        get() = _products
+
+    init {
+        getProducts()
+    }
+
+    private fun getProducts() {
+        val single = ProductsApi.retrofitService.getProducts()
+        disposableObserver = single.subscribe({ response ->
+            _products.value = response.body()
+        }, {})
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposableObserver?.dispose()
+    }
 }
