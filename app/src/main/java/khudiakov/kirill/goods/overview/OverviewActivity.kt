b6ff64524c0134b2ac1cf7f10cf3dc667c6b8e27
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-10 01:32:08 +0300

    Instantiate view model and recycler view

--- app/src/main/java/khudiakov/kirill/goods/overview/OverviewActivity.kt
+++ app/src/main/java/khudiakov/kirill/goods/overview/OverviewActivity.kt
@@ -2,12 +2,39 @@ package khudiakov.kirill.goods.overview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import khudiakov.kirill.goods.R
+import khudiakov.kirill.goods.databinding.ActivityOverviewBinding
 
 class OverviewActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: OverviewViewModel
+    private lateinit var binding: ActivityOverviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_overview)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_overview)
+        binding.lifecycleOwner = this
+
+        viewModel = ViewModelProviders.of(this)[OverviewViewModel::class.java]
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        val viewAdapter = ProductsListAdapter(ProductsListAdapter.OnClickListener {})
+        val viewManager = LinearLayoutManager(this)
+
+        viewModel.products.observe(this, Observer { list ->
+            viewAdapter.submitList(list)
+        })
+
+        binding.list.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
 }
