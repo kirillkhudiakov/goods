f68423a5f83ff08f88a4500394c99ec3b84c9ee8
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-11 05:20:34 +0300

    Create different binding adapter for circular and rectangular image views

--- app/src/main/java/khudiakov/kirill/goods/adapters/BindingAdapters.kt
+++ app/src/main/java/khudiakov/kirill/goods/adapters/BindingAdapters.kt
@@ -4,12 +4,21 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
+@BindingAdapter("circleImageUrl")
+fun bindCircleImage(imageView: ImageView, imageUrl: String?) {
+    imageUrl?.let {
+        Glide.with(imageView.context)
+            .load(imageUrl)
+            .circleCrop()
+            .into(imageView)
+    }
+}
+
 @BindingAdapter("imageUrl")
 fun bindImage(imageView: ImageView, imageUrl: String?) {
     imageUrl?.let {
         Glide.with(imageView.context)
             .load(imageUrl)
-            .circleCrop()
             .into(imageView)
     }
 }
