8b071c0bd5f8c92f2ad1cb0c7e6ef4ffa4dc3dc9
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-11 05:01:30 +0300

    Add binding

--- app/src/main/java/khudiakov/kirill/goods/detail/DetailActivity.kt
+++ app/src/main/java/khudiakov/kirill/goods/detail/DetailActivity.kt
@@ -2,12 +2,18 @@ package khudiakov.kirill.goods.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import khudiakov.kirill.goods.R
+import khudiakov.kirill.goods.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+
+        val id = intent.extras?.getLong("Key")
     }
 }
