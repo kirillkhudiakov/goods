2689f565da1d193edb290cb4c799f955ac23a95b
Kirill Khudiakov <khudiakovk@gmail.com>
2019-11-11 05:00:10 +0300

    Remove view model, use repository instead

--- app/src/main/java/khudiakov/kirill/goods/overview/OverviewActivity.kt
+++ app/src/main/java/khudiakov/kirill/goods/overview/OverviewActivity.kt
@@ -5,16 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import khudiakov.kirill.goods.R
+import khudiakov.kirill.goods.adapters.ProductsListAdapter
+import khudiakov.kirill.goods.data.ProductsRepository
 import khudiakov.kirill.goods.databinding.ActivityOverviewBinding
 import khudiakov.kirill.goods.detail.DetailActivity
 
 class OverviewActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: OverviewViewModel
     private lateinit var binding: ActivityOverviewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,17 +23,17 @@ class OverviewActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_overview)
         binding.lifecycleOwner = this
 
-        viewModel = ViewModelProviders.of(this)[OverviewViewModel::class.java]
         setupRecyclerView()
     }
 
     private fun setupRecyclerView() {
-        val viewAdapter = ProductsListAdapter(ProductsListAdapter.OnClickListener { id ->
-            navigateToDetailActivity(id)
-        })
+        val viewAdapter =
+            ProductsListAdapter(ProductsListAdapter.OnClickListener { id ->
+                navigateToDetailActivity(id)
+            })
         val viewManager = LinearLayoutManager(this)
 
-        viewModel.products.observe(this, Observer { list ->
+        ProductsRepository.products.observe(this, Observer { list ->
             viewAdapter.submitList(list)
         })
 
